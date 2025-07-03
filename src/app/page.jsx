10a0b5d2958039f4662c7f2ec5c67ae9745f45c0
drugcarts <drugcartspro@gmail.com>
2025-07-03 16:57:57 +0400

    nextapp

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline bg-bgcolor text-center text-white">
-      Hello world!
+      Hello world! Velvizhi Arunkumar Jeswanth
     </h1>
   );
 }
